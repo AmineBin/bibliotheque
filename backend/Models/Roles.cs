6469841eea50ed589b5fb9e6e4242e874e26579f
amine <aminebin2004@gmail.com>
2025-11-26 19:24:38 +0100

    Ajout des classes Roles et Users

---
+++ backend/Models/Roles.cs
@@ -0,0 +1,13 @@
+﻿namespace DefaultNamespace;
+
+public class Roles
+{
+    public enum RoleName
+    {
+        Librarian,
+        Student,
+        Teacher
+    }
+    public Guid Id { get; set; }
+    public RoleName Name { get; set; }
+}
---
+++ backend/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace DefaultNamespace;
+
+public class Users
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public IEnumerable<Role> Role { get; set; }
+    
+}
